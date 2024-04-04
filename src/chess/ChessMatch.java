ccecf3df261ed2b4d0d91519d85307d53599e363
Guilherme <guilherme_riba@hotmail.com>
2024-04-04 16:05:01 -0300

    Fix - better treatment to promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,5 @@
 package chess;
 
-import java.lang.foreign.ValueLayout.OfBoolean;
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
